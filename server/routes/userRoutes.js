56aabed9198b598843937490afd6cae6c2717b4a
OJSimp <spencersimpson332@gmail.com>
2023-03-25 18:05:41 +1300

    updates to the access nav links

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+
+const express = require("express")
+const router = express.Router()
+
+module.exports = router
+
+const { loginUser, signupUser } = require("../controllers/userController")
+
+ // Log In route - login page
+ router.post('/login', loginUser)
+
+
+ // Sign up route - signup page
+ router.post('/signup', signupUser)
+
+
