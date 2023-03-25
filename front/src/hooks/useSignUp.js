1c70de96a5c37edafa3d4b13e8000a377b02cbf8
OJSimp <spencersimpson332@gmail.com>
2023-03-26 12:05:35 +1300

    sign up user with error handling

---
+++ front/src/hooks/useSignUp.js
@@ -0,0 +1,51 @@
+
+import { useState } from "react"
+import { useAuthContext } from "./useAuthContext"
+
+export const useSignUp = () => {
+ //if there is an error  // is loading 
+ const [error, setError] = useState(null)
+ const [loading, setLoading] = useState(null)
+
+ // use the dispatch function to fire the right "case"
+ const {dispatch} = useAuthContext()
+
+ //sending data to databse using a post request 
+ const signup = async (firstName, lastName, email, password) => {
+  // starting request - so is loading & reset errors to null 
+  setLoading(true)
+  setError(null)
+
+  const response = await fetch("http://localhost:8001/users/signup/", {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify({ firstName, lastName, email, password })
+ })
+
+ // send back webtoken if sucessful 
+ const json = await response.json()
+ 
+
+ if(!response.ok){
+  // if not sucessful - send back error and finished loading 
+ setLoading(false)
+ setError(json.error)
+
+ } if(response.ok){
+ // if sucessful - save user to local storage - (log user in)
+  // userController.js returns in json email and token, from backend
+  localStorage.setItem('user', JSON.stringify(json))
+
+ // update authContext using authContext hook 
+  // AuthContext.js run case LogIn - send response as json
+  dispatch({type: "LOGIN", paylaod: json } )
+
+ // finsihed loading now false
+ setLoading(false)
+ }
+ }
+
+
+ // we need to return the "signup function", the "loading state" and the "error state"
+ return { signup, loading, error }
+}
