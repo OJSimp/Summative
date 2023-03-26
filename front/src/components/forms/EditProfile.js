eb4ed43b18ae128f1f1861410c9a473deaadc898
OJSimp <spencersimpson332@gmail.com>
2023-03-27 11:15:55 +1300

    stuff for amiee

--- front/src/components/forms/EditProfile.js
+++ front/src/components/forms/EditProfile.js
@@ -14,34 +14,6 @@ const EditProfile = () => {
 
  const { user } = useAuthContext()
 
-// there is a delay between when the page laods and the local storage email
-
- useEffect(() => {
-  if (user){
-  console.log("Delivery for Amiee:", user.email)
-  setUserEmail(user.email)
-
-  // once there is a user get their information from server
-  getUser()
-  } else {
-  console.log("no user yet")
-
- }})
-
-// get request to server to get the user information 
-
-const getUser = async () => {
-
-// user email comes from the seteUserEmail useState()
-const response = await fetch(`http://localhost:8001/users/${userEmail}`, {method: "GET"})
-const userResponse = await response.json()
-
-const user = userResponse[0]
-console.log(user)
-
-}
-
-
 
  return (
   <div className="edit-profile">
