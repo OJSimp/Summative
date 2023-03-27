639a3f7d306fbe987dd70e9968c7f1e844dbf3b9
OJSimp <spencersimpson332@gmail.com>
2023-03-27 13:02:31 +1300

    display cards branch setup

--- front/src/pages/Search.js
+++ front/src/pages/Search.js
@@ -1,8 +1,19 @@
 
+import { useState, useEffect } from "react"
 
+import { Link, Outlet } from "react-router-dom"
 
+import { useAuthContext } from "../hooks/useAuthContext"
 
 const Search = () => {
+
+ 
+
+ return(
+  <div className="search">
+   <p>search</p>
+  </div>
+  )
 
 
 }
