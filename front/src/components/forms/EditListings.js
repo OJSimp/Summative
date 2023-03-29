1a215f736982fe76c962570892fa4b5e047d8320
OJSimp <spencersimpson332@gmail.com>
2023-03-29 19:09:16 +1300

    details for edit listing form

--- front/src/components/forms/EditListings.js
+++ front/src/components/forms/EditListings.js
@@ -1,12 +1,40 @@
+import { useParams } from "react-router-dom"
 
+import { useState, useEffect } from "react"
+
+import { useAuthContext } from "../../hooks/useAuthContext"
+import { useGetUser } from "../../hooks/useGetUser"
 
 const EditListings = () => {
 
+ const listingId = useParams().listingsId
+
+ const { user } = useAuthContext()
+
+ const { userDetails, ID, firstName, lastName } = useGetUser()
  
+ console.log(listingId)
+
+  // Render Logged-In user Details 
+  useEffect(() => {
+
+  if(user){
+  const userEmail = user.email
+  userDetails(userEmail)
+  }
+
+  }, [user])
+
+  // Render listing details
+  useEffect(() => {
+
+  
+  }, [listingId])
+
 
  return (
   <div className="">
-    
+    EDIT LISTINGS FORM
   </div>
 
   )
--- front/src/pages/YourListings.js
+++ front/src/pages/YourListings.js
@@ -1,14 +1,11 @@
-
-
 import { Link, Outlet } from "react-router-dom"
 
 import ListingCard from "../components/cards/ListingCard"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import { useAuthContext } from "../hooks/useAuthContext"
 import { useGetUser } from "../hooks/useGetUser"
-import { useEffect } from "react"
 
 
 const YourListings = () => {
@@ -19,7 +16,7 @@ const YourListings = () => {
 
   const { userDetails, ID, firstName, lastName } = useGetUser()
 
-  const editListingsPage = "your-listings"
+  const editListingsPage = "edit-listings"
 
   useEffect(() => {
 
@@ -31,11 +28,12 @@ const YourListings = () => {
   }, [user])
 
   useEffect(() => {
-  console.log("next")
 
   if(ID){
+
     const returnListingData = async () => {
-    const resposne = await fetch(`http://localhost:8001/edit-listings/${ID}`, {method: "GET"})
+     
+    const resposne = await fetch(`http://localhost:8001/your-listings/${ID}`, {method: "GET"})
     const data = await resposne.json()
     const dataArray = data
     console.log(dataArray)
