deab0812c90f96d78be522e07ab88288c0350cdb
Spencer Simpson <82491466+OJSimp@users.noreply.github.com>
2023-04-03 14:04:00 +1200

    Revert "Revert "Global CSS and page setup for headerNav and About page""

--- front/src/pages/Profile.js
+++ front/src/pages/Profile.js
@@ -1,37 +1,45 @@
 import "./Profile.scss";
 
-import NavProfile from "../components/navigation/ProfileNav";
+import NavProfile from "../components/navigation/ProfileNav"
+import EditProfile from "../components/forms/EditProfile"
 
-import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom"
 
-import { Link, Outlet } from "react-router-dom";
+
+import Media from 'react-media'; // add Media
 
 import Media from "react-media"; // add Media
 
 const Profile = () => {
-  return (
-    <div className="profile">
-      {/* Insert header here */}
 
-      <Media query="(max-width: 599px)">
-        {(matches) =>
-          matches ? (
-            <div className="mobile">
-              <NavProfile />
-            </div>
-          ) : (
-            <div className="desktop">
-              <NavProfile />
 
-              <Outlet />
-            </div>
-          )
-        }
-      </Media>
+ return (
+  <div className="profile">
+  <p>Profile</p>
 
-      {/* Display Profile Edit and Post Edit underneath hide on keyframes  */}
-    </div>
-  );
-};
+{/* --MEDIA QUERY START-- */}
+       < Media query="(max-width: 599px)" >
+          {matches => matches ? 
+          ( <div className="mobile">
+            < NavProfile />  
+            </div>) 
+          : 
+          ( <div className="desktop">
+            < NavProfile />
+            < Outlet />
+            </div>)}
+        </Media> 
+{/* --MEDIA QUERY END-- */}
 
-export default Profile;
+  < EditProfile />
+   {/* Display Profile Edit and Post Edit underneath hide on keyframes  */}
+
+  </div>
+  
+  )
+
+}
+
+
+export default Profile
+
