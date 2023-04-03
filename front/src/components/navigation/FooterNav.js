2137f59ad959a514ce3d0dec3300166e0474aa9c
OJSimp <spencersimpson332@gmail.com>
2023-04-03 19:15:33 +1200

    rotes are working

--- front/src/components/navigation/FooterNav.js
+++ front/src/components/navigation/FooterNav.js
@@ -1,41 +1,61 @@
+import "./FooterNav.scss";
 
-import "./FooterNav.scss"
-
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 import { FaHome, FaSearch, FaPlus, FaUser } from "react-icons/fa";
 
 const FooterNav = () => {
+  return (
+    <div className="nav__footer">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? "btn-nav btn-nav--active" : "btn-nav btn-nav--inactive"
+        }
+      >
+        <p>Home</p>
+        <span className="btn-nav__icon">
+          <FaHome />
+        </span>
+      </NavLink>
 
-return (
- <div className="nav__footer">
-  <NavLink to="/sign-up" className={({ isActive }) =>(isActive ? 'btn-nav btn-nav--active' : 'btn-nav btn-nav--inactive')}><p>Home</p> 
-   <span className="btn-nav__icon">
-    < FaHome /> 
-   </span>
-  </NavLink>
+      <NavLink
+        to="/search"
+        className={({ isActive }) =>
+          isActive ? "btn-nav btn-nav--active" : "btn-nav btn-nav--inactive"
+        }
+      >
+        <p>Search</p>
+        <span className="btn-nav__icon">
+          <FaSearch />
+        </span>
+      </NavLink>
 
-  <NavLink to="/search" className={({ isActive }) =>(isActive ? 'btn-nav btn-nav--active' : 'btn-nav btn-nav--inactive')}><p>Search</p> 
-   <span className="btn-nav__icon">
-    < FaSearch /> 
-   </span>
-  </NavLink>
+      <NavLink
+        to="/upload-art"
+        className={({ isActive }) =>
+          isActive ? "btn-nav btn-nav--active" : "btn-nav btn-nav--inactive"
+        }
+      >
+        <p>Add art</p>
+        <span className="btn-nav__icon">
+          <FaPlus />
+        </span>
+      </NavLink>
 
-  <NavLink to="/upload-art" className={({ isActive }) =>(isActive ? 'btn-nav btn-nav--active' : 'btn-nav btn-nav--inactive')}><p>Add art</p>  
-   <span className="btn-nav__icon">
-    < FaPlus /> 
-   </span>
-  </NavLink>
+      <NavLink
+        to="/profile"
+        className={({ isActive }) =>
+          isActive ? "btn-nav btn-nav--active" : "btn-nav btn-nav--inactive"
+        }
+      >
+        <p>Profile</p>
+        <span className="btn-nav__icon">
+          <FaUser />
+        </span>
+      </NavLink>
+    </div>
+  );
+};
 
-  <NavLink to="/profile" className={({ isActive }) =>(isActive ? 'btn-nav btn-nav--active' : 'btn-nav btn-nav--inactive')}><p>Profile</p> 
-   <span className="btn-nav__icon">
-    < FaUser />
-   </span>
-  </NavLink>
- </div>
- 
- )
-
-}
-
-export default FooterNav
+export default FooterNav;
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,38 +1,14 @@
 import "./Home.scss";
 
 import HeaderNav from "../components/navigation/HeaderNav";
-import FooterNav from "../components/navigation/FooterNav"
-
-
-import AppRoutes from "../Routes"
-import Media from 'react-media'; // add Media
-
+import FooterNav from "../components/navigation/FooterNav";
 
 const Home = () => {
+  return (
+    <div className="home-wrapper">
+      <p>Home</p>
+    </div>
+  );
+};
 
- return(
-  <div className="home-wrapper">
-    < HeaderNav />
-    <p>Home</p>
-
-
-{/* --MEDIA QUERY START-- */}
-< Media query="(max-width: 599px)" >
-          {matches => matches ? 
-          ( <div className="mobile">
-            </div>) 
-          : 
-          ( <div className="desktop">
-            </div>)}
- </Media> 
-{/* --MEDIA QUERY END-- */}
-
-      < AppRoutes />
-      < FooterNav />
- 
- </div>
-  )
-}
-
-
-export default Home
+export default Home;
