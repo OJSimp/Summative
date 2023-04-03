1d26ee48e8dac9d84ce76c01bd3ecc7e9bd20287
OJSimp <spencersimpson332@gmail.com>
2023-04-03 14:18:14 +1200

    fix errors in commit

--- front/src/pages/Profile.js
+++ front/src/pages/Profile.js
@@ -1,45 +1,38 @@
 import "./Profile.scss";
 
-import NavProfile from "../components/navigation/ProfileNav"
-import EditProfile from "../components/forms/EditProfile"
+import NavProfile from "../components/navigation/ProfileNav";
+import EditProfile from "../components/forms/EditProfile";
 
-import { Outlet } from "react-router-dom"
-
-
-import Media from 'react-media'; // add Media
+import { Outlet } from "react-router-dom";
 
 import Media from "react-media"; // add Media
 
 const Profile = () => {
+  return (
+    <div className="profile">
+      <p>Profile</p>
 
+      {/* --MEDIA QUERY START-- */}
+      <Media query="(max-width: 599px)">
+        {(matches) =>
+          matches ? (
+            <div className="mobile">
+              <NavProfile />
+            </div>
+          ) : (
+            <div className="desktop">
+              <NavProfile />
+              <Outlet />
+            </div>
+          )
+        }
+      </Media>
+      {/* --MEDIA QUERY END-- */}
 
- return (
-  <div className="profile">
-  <p>Profile</p>
+      <EditProfile />
+      {/* Display Profile Edit and Post Edit underneath hide on keyframes  */}
+    </div>
+  );
+};
 
-{/* --MEDIA QUERY START-- */}
-       < Media query="(max-width: 599px)" >
-          {matches => matches ? 
-          ( <div className="mobile">
-            < NavProfile />  
-            </div>) 
-          : 
-          ( <div className="desktop">
-            < NavProfile />
-            < Outlet />
-            </div>)}
-        </Media> 
-{/* --MEDIA QUERY END-- */}
-
-  < EditProfile />
-   {/* Display Profile Edit and Post Edit underneath hide on keyframes  */}
-
-  </div>
-  
-  )
-
-}
-
-
-export default Profile
-
+export default Profile;
