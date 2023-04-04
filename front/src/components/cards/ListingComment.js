eab060674261f8f7f853c749b0b55dadd0e0a2b9
ZeeshanAliii <317Zeeali@gmail.com>
2023-04-04 12:36:03 +1200

    edit comment modal completed

--- front/src/components/cards/ListingComment.js
+++ front/src/components/cards/ListingComment.js
@@ -1,11 +1,14 @@
 import "./ListingComment.scss";
-
+import { useState } from "react";
 import { AiOutlineEdit, AiFillDelete } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 
 const ListingComments = (props) => {
   const handleEdit = () => {
     console.log("Edit Comment");
   };
+
+  const [modal, setModal] = useState(false);
 
   const handleDelete = async (e) => {
     const commentID = e.target.id;
@@ -20,6 +23,11 @@ const ListingComments = (props) => {
     const data = await response.json();
     console.log(data);
     props.reloadData();
+  };
+  // modal
+
+  const toggleModal = () => {
+    setModal(!modal);
   };
 
   const commentsArray = props.comments;
@@ -38,7 +46,7 @@ const ListingComments = (props) => {
         {/* if the signed in user created the comment show the edit and delete button */}
         {commentCreator == props.id ? (
           <div className="card-comment__utility">
-            <button className="btn-text" onClick={handleEdit}>
+            <button className="btn-text" onClick={toggleModal}>
               Edit <AiOutlineEdit />
             </button>
             <button
@@ -53,8 +61,44 @@ const ListingComments = (props) => {
       </div>
     );
   });
+  {
+  }
 
-  return <div>{commentCards}</div>;
+  return (
+    <div>
+      {commentCards}
+      {/* Modal HTML */}
+      {modal ? (
+        <div className="edit-modal__overlay">
+          <div className="modal">
+            <header className="modal-top-header_element">
+              <h2 className="edit-header_text">Edit your comment</h2>
+              <button onClick={toggleModal} className="btn-icon">
+                <MdClose />
+              </button>
+            </header>
+            <body className="modal-body_element">
+              <input
+                placeholder="Comment Details"
+                className="edit-comment_input"
+                type="text"
+                name=""
+                id=""
+              />
+            </body>
+            <footer className="modal-footer_element">
+              <button
+                onClick={handleEdit}
+                className="btn-primary edit-button_size "
+              >
+                edit
+              </button>
+            </footer>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default ListingComments;
