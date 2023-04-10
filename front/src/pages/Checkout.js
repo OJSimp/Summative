ae74c864582e31ee4467b0502be629b4f784e2ec
Aimee Ralfini <107170054+Aimee-Ralfini@users.noreply.github.com>
2023-04-11 11:57:00 +1200

    Zeeshans updated checkout pages

--- front/src/pages/Checkout.js
+++ front/src/pages/Checkout.js
@@ -1,12 +1,39 @@
 import "./Checkout.scss";
-//  ZEE put your code here
+
 
 const Checkout = () => {
-  return (
-    <div className="checkout-page">
-      <p>CHECKOUT PAGE</p>
+return (
+
+<div className="checkout-wrapper">
+    <div className="title">
+    <h2>View cart</h2>
     </div>
-  );
-};
+
+    <div className="view-cart_container">
+        <div className="artwork-item-in_cart">
+        <p>Artwork name 1 </p>
+        <img className="img-placeholder" src="" alt="1" />
+        <p>$0.00</p>
+        </div>
+
+        <div className="artwork-item-in_cart">
+        <p>Artwork name 2 </p>
+        <img className="img-placeholder" src="" alt="2" />
+        <p>$0.00</p>
+        </div>
+
+        <div className="price-total_container">
+        <h4>$00.00</h4>
+        </div>
+    </div>
+
+    <div className="buttons_container">
+    <button className="btn-primary checkout_button">Go to checkout </button>
+    <button className="btn-outline">continue shopping</button>
+    </div>
+</div>
+// End of checkout wrapper
+
+)};
 
 export default Checkout;
